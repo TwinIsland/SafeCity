c49d7c340e0ecef2fae6b9223976b07ef6865e82
Huang Tianyi <wyatt15378200925@gmail.com>
2020-10-23 18:37:20 +0800

    修改测试类

--- src/test/java/com/dtone/ssm/SsmApplicationTestsHuang.java
+++ src/test/java/com/dtone/ssm/SsmApplicationTestsHuang.java
@@ -20,61 +20,12 @@ import java.util.List;
 class SsmApplicationTests {
 
     @Autowired
-    private IUserDao userDao;
-    @Autowired
-    private ILogDao logDao;
-    @Autowired
     private IExpertDao expertDao;
     @Autowired
     private IEventDao eventDao;
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void selectUserTest() {
-        UserEntity userEntity = userDao.selectUser("superadmin", "superadmin");
-        //断言
-        Assert.notNull(userEntity);
-    }
-
-    @Test
-    void selectAllUserTest() {
-        List<UserEntity> userEntities = userDao.selectAllUser();
-        //断言
-        Assert.notNull(userEntities);
-    }
-
-    @Test
-    void insertUserTest() {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setName("1");
-        userEntity.setLevel("1");
-        userEntity.setPassword("1");
-        userEntity.setRealname("1");
-        userDao.insertUser(userEntity);
-    }
-
-    @Test
-    void deleteUserTest() {
-        userDao.deleteUser(7);
-    }
-
-    @Test
-    void selectAllLogTest() {
-        List<LogEntity> logEntities = logDao.selectAllLog();
-        Assert.notNull(logEntities);
-    }
-
-    void slectLogByDateTest(Date date1, Date date2) {
-        List<LogEntity> logEntities = logDao.selectLogByDate(date1, date2);
-        Assert.notNull(logEntities);
-    }
-
-    @Test
-    void insertLogTest() {
-        LogEntity logEntity = new LogEntity();
     }
 
     @Test
