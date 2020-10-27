2d3bc3dec64de0f7e3bad65bc35d7aa4a1106616
Huang Tianyi <15378200925@163.com>
2020-10-27 14:44:22 +0800

    login.html 导航栏无法下拉bug

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,19 +1,29 @@
 $(function () {
+    isLogin()
     $("#submit").click(function () {
-        $.ajax({
-            url:"/user/login",
-            type:"post",
-            data:{
-                name:$("#usrName").val,
-                password:$("#usrPsw").val,
-            },
-            success:function (result) {
-                if(result === "success"){
-                    alert("登陆成功")
-                }else{
-                    alert("登陆失败")
+        if(!validate()){
+            $("#status").empty().append("<p style=\"color: red;font-size: 13px\">&nbsp;&nbsp;&nbsp;&nbsp;验证码错误！</p>")
+            createCode();
+            $("#capCode").empty()
+        }else {
+            $.ajax({
+                url: "user/login",
+                type: "POST",
+                data: {
+                    name: $("#usrName").val(),
+                    password: $("#usrPsw").val(),
+                },
+                success: function (result) {
+                    if (result === "success") {
+                        $("#status").empty().append("<p style=\"color: forestgreen;font-size: 13px\">&nbsp;&nbsp;&nbsp;&nbsp;登陆成功，欢迎！</p>")
+                        window.location.href = 'index.html';
+                    } else {
+                        $("#status").empty().append("<p style=\"color: red;font-size: 13px\">&nbsp;&nbsp;&nbsp;&nbsp;用户名或密码错误，请重试！</p>")
+                        document.getElementById("usrName").value = "";
+                        document.getElementById("usrPsw").value = "";
+                    }
                 }
-            }
-        })
+            })
+        }
     })
 })
