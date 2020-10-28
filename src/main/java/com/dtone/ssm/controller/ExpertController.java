0ec51a381c69533a7fd8c8f4897623a8087ba9b6
liyanjie123 <315149202@qq.com>
2020-10-28 14:23:49 +0800

    增加拦截器

--- src/main/java/com/dtone/ssm/controller/ExpertController.java
+++ src/main/java/com/dtone/ssm/controller/ExpertController.java
@@ -4,7 +4,9 @@ import com.dtone.ssm.entity.BugEntity;
 import com.dtone.ssm.entity.ExpertEntity;
 import com.dtone.ssm.entity.UserEntity;
 import com.dtone.ssm.service.IExpertService;
+import com.dtone.ssm.service.ILogService;
 import com.dtone.ssm.service.IUserService;
+import com.dtone.ssm.util.TimeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,19 +22,43 @@ import java.util.List;
  * @desc
  */
 @RestController
+@RequestMapping("html/")
 @Slf4j  //获取日志对象
 public class ExpertController
 {
-        @Autowired
-        private IExpertService iExpertService;
-        @RequestMapping("info/expert")
-        public List<ExpertEntity> getExpert() {
-            log.info("收到请求:info/expert");
-            List<ExpertEntity> expertEntities = iExpertService.selectAllExpert();
-            log.info("返回对象");
-            return expertEntities;
+    @Autowired
+    private IExpertService iExpertService;
+    @Autowired
+    private ILogService iLogService;
+
+    @RequestMapping("info/expert")
+    public List<ExpertEntity> getExpert()
+    {
+        log.info("收到请求:info/expert");
+        List<ExpertEntity> expertEntities = iExpertService.selectAllExpert();
+        log.info("返回对象");
+        return expertEntities;
+    }
+
+    @RequestMapping("info/searchedExpert")
+    public List<ExpertEntity> getSearchedExpert(String name, String expertise, String company)
+    {
+        log.info("收到请求:info/searchedExpert");
+
+        List<ExpertEntity> expertEntities = iExpertService.searchExpert(name, expertise, company);
+        log.info("返回对象2");
+        return expertEntities;
+    }
+    @RequestMapping("manage/addExpert.action")
+    public String addExpert(String name,String company,String expertise,String post,String phone)
+    {
+        if (name.equals("") || company.equals("") || expertise.equals("") || post.equals("") || phone.equals(""))
+        {
+            iLogService.userLogin("插入区域数据：" + name + "【状态：失败】", TimeUtil.getCurTime());
+            return "false";
         }
-
-
-
+        iLogService.userLogin("插入区域数据：" + name + "【状态：成功】", TimeUtil.getCurTime());
+        iExpertService.addExpert(new ExpertEntity(name, company, expertise, post, phone));
+        return name;
+    }
 }
--- src/main/java/com/dtone/ssm/service/IExpertService.java
+++ src/main/java/com/dtone/ssm/service/IExpertService.java
@@ -5,8 +5,8 @@ import com.dtone.ssm.entity.ExpertEntity;
 import java.util.List;
 
 public interface IExpertService {
-    public void addExpert(ExpertEntity exp);
+    public void addExpert(ExpertEntity expertEntity);
     public void delExpert(int id);
-    public void searchExpert(String name,String expertise,String company);
+    public List<ExpertEntity> searchExpert(String name, String expertise, String company);
     public List<ExpertEntity> selectAllExpert();
 }
